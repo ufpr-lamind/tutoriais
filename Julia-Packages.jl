b96110ae6ab46649b196e18b75e95d4bfb32d177
andrebuhler <andrebuhler@gmail.com>
2016-03-02 10:55:04 -0300

    Cria Julia-Packages

---
+++ Julia-Packages.jl
@@ -0,0 +1,10 @@
+#Versao1.0
+Pkg.clone("git://github.com/kbarbary/TimeIt.jl.git")
+Pkg.update()
+Pkg.add("Gadfly")
+Pkg.add("IJulia")
+Pkg.build("IJulia")
+Pkg.clone("https://github.com/optimizers/CUTEst.jl")
+Pkg.checkout("CUTEst", "develop")
+Pkg.add("Atom")
+Pkg.add("Winston")
---
+++ Julia-Packages.md
@@ -0,0 +1,11 @@
+# Introdução
+O script Julia-Packages.jl instala de forma automatica uma série de pacotes no
+Julia necessários para a utilização do Lamind.
+
+# Lista de pacotes
+- Atom
+- CUTEst
+- Gadfly
+- IJulia
+- TimeIt
+- Winston
