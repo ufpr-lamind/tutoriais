65a9600595b0a9f518ed4085df032f419941a267
andrebuhler <andre_buhler@hotmail.com>
2016-11-30 14:50:25 -0200

    Atualiza Julia-Packages.jl e .md

--- Julia-Packages.jl
+++ Julia-Packages.jl
@@ -1,5 +1,6 @@
-#Versao1.0
-#Instalação feito no usuário comum
+#Versao2.3
+# No Julia no usuário comum
+##Misc
 Pkg.clone("git://github.com/kbarbary/TimeIt.jl.git")
 Pkg.update()
 Pkg.add("Gadfly")
@@ -9,3 +10,20 @@ Pkg.clone("https://github.com/optimizers/CUTEst.jl")
 Pkg.checkout("CUTEst", "develop")
 Pkg.add("Atom")
 Pkg.add("Winston")
+##Plots
+Pkg.add("Plots")
+Pkg.add("GR")
+Pkg.add("PyPlot")
+Pkg.build("PyCall")
+Pkg.checkout("Plots", "dev")
+Pkg.add("Immerse")
+Pkg.add("UnicodePlots")
+Pkg.add("Qwt")
+Pkg.add("RDatasets")
+Pkg.add("Images")
+Pkg.add("ImageMagick")
+##JuMP
+Pkg.add("JuMP")
+Pkg.add("Ipopt")
+Pkg.add("Cbc")
+Pkg.add("Clp")
--- Julia-Packages.md
+++ Julia-Packages.md
@@ -3,9 +3,23 @@ O script Julia-Packages.jl instala de forma automatica uma série de pacotes no
 Julia necessários para a utilização do Lamind.
 
 # Lista de pacotes
-- Atom
-- CUTEst
-- Gadfly
-- IJulia
-- TimeIt
-- Winston
+  - Atom
+  - Cbc
+  - Clp
+  - CUTEst
+  - Gadfly
+  - GR
+  - IJulia
+  - ImageMagick
+  - Images
+  - Immerse
+  - Ipopt
+  - JuMP
+  - Plots
+  - PyCall
+  - PyPlot
+  - Qwt
+  - Rdatasets
+  - TimeIt
+  - UnicodePlots
+  - Winston
