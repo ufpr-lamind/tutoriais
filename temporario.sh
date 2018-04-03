e317a37ead072951e4c1d845b7065fc6725a28ac
Renan Domingues <renan_oclides@hotmail.com>
2018-04-03 19:29:12 -0300

    Update temporario.sh

--- temporario.sh
+++ temporario.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 cd $HOME
 
-sudo apt-get install openjdk-8-jdk
+sudo apt-get install openjdk-8-jdk -y
 sudo R CMD javareconf
 
 sudo apt-get install libapparmor-dev libpoppler-glib-dev libssl-dev libcurl4-openssl-dev libxml2-dev libwebp-dev libpoppler-cpp-dev libmagick++-dev libtesseract-dev libleptonica-dev mesa-common-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev -y
